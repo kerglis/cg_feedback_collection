144f54dc0453b3d47b13e3e0881d4a3fffd80021
Kristaps Erglis <kristaps.erglis@gmail.com>
2015-01-13 16:03:26 +0200

    demo env + https

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.dragonfly_host = 'http://feedback.codeguard.com'
+  config.dragonfly_host = 'https://feedback.codeguard.com'
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
